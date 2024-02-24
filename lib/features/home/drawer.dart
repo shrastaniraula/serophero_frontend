07d5408ce91515b3c26820ee3b690b1b543c8608
shrastaniraula <shrastatsarhs58@gmail.com>
2024-02-24 21:39:23 +0545

    new changes

---
+++ lib/features/home/drawer.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: MediaQuery.of(context).size.width / 1.2,
+    );
+  }
+}
--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -9,20 +9,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Row(
               children: [
-                Image(
-                    height: 22,
-                    width: 22,
-                    color: Theme.of(context).colorScheme.tertiary,
-                    image: const AssetImage("assets/images/logos/menu.png")),
+                Builder(
+                  builder: (context) => GestureDetector(
+                    onTap: () {
+                      Scaffold.of(context).openDrawer();
+                    },
+                    child: Image(
+                      height: 22,
+                      width: 22,
+                      color: Theme.of(context).colorScheme.tertiary,
+                      image: const AssetImage("assets/images/logos/menu.png"),
+                    ),
+                  ),
+                ),
                 SizedBox(width: 20),
                 Text(
                   "Hi, Shrasta",
@@ -33,10 +44,11 @@ class _HomeState extends State<Home> {
             Row(
               children: [
                 Image(
-                    height: 28,
-                    width: 28,
-                    color: Theme.of(context).colorScheme.tertiary,
-                    image: const AssetImage("assets/images/logos/search.png")),
+                  height: 28,
+                  width: 28,
+                  color: Theme.of(context).colorScheme.tertiary,
+                  image: const AssetImage("assets/images/logos/search.png"),
+                ),
                 SizedBox(width: 15),
                 GestureDetector(
                   onTap: () {
@@ -44,16 +56,19 @@ class _HomeState extends State<Home> {
                       context,
                       GeneratedRoute().onGeneratedRoute(
                         const RouteSettings(
-                            arguments: '', name: '/notification'),
+                          arguments: '',
+                          name: '/notification',
+                        ),
                       ),
                     );
                   },
                   child: Image(
-                      height: 22,
-                      width: 22,
-                      color: Theme.of(context).colorScheme.tertiary,
-                      image: const AssetImage(
-                          "assets/images/logos/notification.png")),
+                    height: 22,
+                    width: 22,
+                    color: Theme.of(context).colorScheme.tertiary,
+                    image: const AssetImage(
+                        "assets/images/logos/notification.png"),
+                  ),
                 ),
               ],
             ),
@@ -62,6 +77,9 @@ class _HomeState extends State<Home> {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
       ),
+      drawer: Drawer(
+          width: MediaQuery.of(context).size.width / 1.1,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero)),
     );
   }
 }
